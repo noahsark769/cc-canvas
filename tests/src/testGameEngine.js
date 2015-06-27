f5a8314a553dfad5d049e5d218e19b52bcb4b717
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-06-27 11:20:03 -0500

    add app-root-path, thats a nice touch

--- tests/src/testGameEngine.js
+++ tests/src/testGameEngine.js
@@ -1,6 +1,10 @@
+let reqlib = require("app-root-path").require;
 let { expect } = require("chai");
-let { GameEngine } = require("../../src/GameEngine");
+let { GameEngine } = reqlib("/src/GameEngine");
 
 describe("GameEngine", () => {
-
+    it("should be available via import", () => {});
+    it("should work with appRootPath imports and chai", () => {
+        expect(1).to.equal(1);
+    });
 });
